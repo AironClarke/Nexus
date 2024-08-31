bf0cd1c250ca6709e2810e29f3c0da30a8d7bcca
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-08-31 18:49:48 +0100

    Added Eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  globals: { React: 'readonly' },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'object-curly-newline': ['error', {
+      ObjectExpression: { multiline: true, minProperties: 5 },
+      ObjectPattern: { multiline: true, minProperties: 5 },
+      ImportDeclaration: { multiline: true, minProperties: 5 },
+      ExportDeclaration: { multiline: true, minProperties: 5 }
+    }]
+  },
+};
