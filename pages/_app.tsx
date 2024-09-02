a6bc513866c1f6d5e7173edbdf5c3200a1badb7d
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-09-02 15:03:45 +0100

    Added meta description.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import 'styles/globals.scss';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { name } from 'package.json';
+import packageInfo from 'package.json';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { name, description } = packageInfo;
+
   return (
     <>
       <Head>
         <title>{name}</title>
+        <meta name="description" content={description} />
       </Head>
       <Component {...pageProps} />
     </>
