e801b4ede6796ae70449646a7e06b221b0c87ca1
Airon Clarke <131386167+AironClarke@users.noreply.github.com>
2024-10-03 22:10:10 +0100

    .

--- components/system/Clock.tsx
+++ components/system/Clock.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import StyledClock from 'styles/components/StyledClock';
 
-const Clock: FC = () => <StyledClock>11:17:22 PM</StyledClock>;
+const Clock: FC = () => <StyledClock>11:17:22 PM </StyledClock>;
 
 export default Clock;
